965edfa300689c76200d46a32db4beb5cfb03289
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2022-10-29 17:43:20 +0200

    preparing for getting all the lyrics

--- scrapping/geniusScrapping.py
+++ scrapping/geniusScrapping.py
@@ -1,32 +1,53 @@
 import lyricsgenius
 import pandas as pd
-
-token = "tLsFmYYTavBNOqshrCO3VbGyyNoqw6lOTSndQP3JRnfWX7hceOzz0HzcdoAgOQUU"
-
-genius = lyricsgenius.Genius(token, timeout=20)
-
-genius.verbose = False  # Turn off status messages
-# Remove section headers (e.g. [Chorus]) from lyrics when searching
-genius.remove_section_headers = True
-# Include hits thought to be non-songs (e.g. track lists)
-genius.skip_non_songs = False
-# Exclude songs with these words in their title
-genius.excluded_terms = ["(Live)"]
-
-data = []
-
-genres = ['rap', 'hip-hop', 'country', 'rock', 'pop', 'r-b']
-for genre in genres:
-    page = 1
-    print(genre)
-    while page < 2:
-        res = genius.tag(genre, page=page)
-        for hit in res['hits']:
-            song_lyrics = genius.lyrics(song_url=hit['url'])
-            data.append([hit['artists'][0], hit['title'], song_lyrics, genre, hit['url']])
-        page = res['next_page']
+import sys
+import progressbar
 
 
-df = pd.DataFrame(data, columns=['artists', 'title', 'lyrics', 'genre', 'url'])
+def get_lyrics(token):
 
-df.to_csv('lyrics_small.csv', index=False, sep='#')
+    genius = lyricsgenius.Genius(token, timeout=400)
+
+    genius.verbose = False  # Turn off status messages
+    # Remove section headers (e.g. [Chorus]) from lyrics when searching
+    genius.remove_section_headers = True
+    # Include hits thought to be non-songs (e.g. track lists)
+    genius.skip_non_songs = False
+    # Exclude songs with these words in their title
+    genius.excluded_terms = ["(Live)"]
+
+    k = 1
+    widgets = ['Getting lyrics: ', progressbar.Percentage(), ' ',
+               progressbar.Bar(marker='=',left='[',right=']'),
+               ' ', progressbar.ETA(), ' ', progressbar.FileTransferSpeed()]
+    bar = progressbar.ProgressBar(widgets=widgets, maxval=8000)
+    bar.start()
+
+    data = []
+    genres = ['rap', 'hip-hop', 'country',
+                'rock', 'pop', 'r-b', 'metal', 'jazz']
+    for genre in genres:
+        page = 1
+        while page:
+            res = genius.tag(genre, page=page)
+            for hit in res['hits']:
+                bar.update(k)
+                song_lyrics = genius.lyrics(song_url=hit['url'])
+                data.append([hit['artists'][0], hit['title'],
+                            song_lyrics, genre, hit['url']])
+                k += 1
+            page = res['next_page']
+
+    df = pd.DataFrame(
+        data, columns=['artist', 'title', 'lyrics', 'genre', 'url'])
+    df.to_csv('lyrics.csv', index=False, sep='#')
+
+    bar.finish()
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 2):
+        print("Usage: python3 geniusScrapping.py <token>")
+        exit(1)
+    get_lyrics(sys.argv[1])
+    exit(1)
